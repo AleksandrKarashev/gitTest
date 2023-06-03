7f1b3dfd985ebf5c0a16cd448b9f8d6e289ab9ac
Alek <aleksandr.karashev@jurishand.com>
2023-06-03 14:02:14 -0300

    test 3

--- test1.js
+++ test1.js
@@ -1,4 +1,14 @@
 var x, y, z; // Declare 3 variables
-x = 5; // Assign the value 5 to x
-y = 6; // Assign the value 6 to y
+x = 11; // Assign the value 5 to x
+y = 2222; // Assign the value 6 to y
 z = x + y; // Assign the sum of x and y to z
+
+var y = 5;
+var x = ++y;
+document.getElementById('demo1').innerHTML = y;
+document.getElementById('demo2').innerHTML = x;
+
+var y = 5;
+var x = ++y;
+document.getElementById('demo1').innerHTML = y;
+document.getElementById('demo2').innerHTML = x;
